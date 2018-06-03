6da47261eda5ea19358b418b4a198d3a142c90cc
Ana-Maria Melinte <34279579+AnMa12@users.noreply.github.com>
2018-06-03 06:57:10 +0300

    most weird error
    subSeq[0] returened is an adrees but in for it is an int

--- 2. Divide et Impera Problems/Longest Increasing Subsequence/main.cpp
+++ 2. Divide et Impera Problems/Longest Increasing Subsequence/main.cpp
@@ -4,6 +4,45 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    /*
+7
+3 4 -1 0 6 2 3
+    */
+
+    int n;
+    cin >> n;
+
+    int v[n], subSeq[n];
+    for(int i = 0; i < n; i++)
+        cin >> v[i];
+
+    //intializam toate poz cu 1
+    //pt ca subMax e minim 1
+    for(int i = 0; i < n; i++)
+        subSeq[n] = 1;
+
+        for(int i = 0; i < n; i++)
+        cout << subSeq[n] << " ";
+
+    int i = 1, j = 0;
+    while(i<=n) {
+        while(i!=j) {
+            cout<<endl<<"i = " << i << " j= "<<j<<endl;
+
+            for(int i = 0; i < n; i++)
+            cout << subSeq[n] << " ";
+
+            if(v[j] < v[i])
+                subSeq[i] = subSeq[j] + 1;
+                cout<<"*" << subSeq[0] <<"-" << subSeq[1] << "*" << endl;
+            j++;
+        }
+        j = 0;
+        i++;
+    }
+
+    for(int i = 0; i < n; i++)
+        cout << subSeq[n] << " ";
+
     return 0;
 }
