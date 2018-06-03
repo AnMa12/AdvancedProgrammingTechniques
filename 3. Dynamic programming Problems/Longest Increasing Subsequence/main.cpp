27468ec9d41395bbe3bf3adfcd43079dd33a72b6
Ana-Maria Melinte <34279579+AnMa12@users.noreply.github.com>
2018-06-03 07:09:25 +0300

    Fixed: for
    (int i = 0; i < n; i++) subSeq[n] = 1;
    so just sS[n] had a value

--- 3. Dynamic programming Problems/Longest Increasing Subsequence/main.cpp
+++ 3. Dynamic programming Problems/Longest Increasing Subsequence/main.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
-int main()
-{
-/*
+/*Input data example:
 7
 3 4 -1 0 6 2 3
-*/
+R: -1 0 2 3 => 4*/
+
+int main()
+{
+
 
     int n;
     cin >> n;
@@ -19,13 +22,11 @@ int main()
     //intializam toate poz cu 1
     //pt ca subMax e minim 1
     for(int i = 0; i < n; i++)
-        subSeq[n] = 1;
+        subSeq[i] = 1;
 
     int i = 1, j = 0;
     while(i<=n) {
         while(i!=j) {
-            cout<<endl<<"i = " << i << " j= "<<j<<endl;
-            cout<<"*" << subSeq[0] <<"-" << subSeq[1] << "*" << endl;
             if(v[j] < v[i])
                 subSeq[i] = subSeq[j] + 1;
             j++;
@@ -34,8 +35,12 @@ int main()
         i++;
     }
 
+    int maxi = 0;
     for(int i = 0; i < n; i++)
-        cout << subSeq[n] << " ";
+        if(subSeq[i] > maxi)
+            maxi = subSeq[i];
+
+    cout << maxi;
 
     return 0;
 }
