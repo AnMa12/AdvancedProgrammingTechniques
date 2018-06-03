7fa46d786e795ec7c3cf7943983bc5e5e2ada4e9
Ana-Maria Melinte <34279579+AnMa12@users.noreply.github.com>
2018-06-03 03:56:12 +0300

    Error
    i...
    1i-b-b-b-w-
    2w*
    3b~
    
    b...
    3b~0 0 1 1
    0 0 1 0
    1 1 1 1
    1 1 1 1

--- LaboratoryTest/Ex1/main.cpp
+++ LaboratoryTest/Ex1/main.cpp
@@ -1,9 +1,74 @@
-#include <iostream>
+#include<iostream>
+#include<fstream>
 
 using namespace std;
+ifstream f("date.in");
 
-int main()
-{
-    cout << "Hello world!" << endl;
-    return 0;
+int n, i = 0;
+char v[100];
+int matrix[100][100];
+
+int decodare(int litera) {
+    if(litera == 'w')
+        return 0;
+    if(litera == 'b')
+        return 1;
+}
+
+int matrice(int x, int y, int marime) {
+
+    if(marime == 2) {
+        if(v[i] == 'i') {
+                cout<<endl; cout<<"1";
+           matrix[x][y] = decodare(v[i+1]);
+           matrix[x][y+1] = decodare(v[i+2]);
+           matrix[x+1][y] = decodare(v[i+3]);
+           matrix[x+1][y+1] = decodare(v[i+4]);
+            for(int j = i; j < i+5; j++)
+                cout<<v[j]<< "-";
+           i += 5;
+        } else if(v[i] == 'w') {
+            cout<<endl;
+            cout<<"2";
+           matrix[x][y] = 0;
+           matrix[x][y+1] = 0;
+           matrix[x+1][y] = 0;
+           matrix[x+1][y+1] = 0;
+           cout<<v[i]<<"*";
+           i += 1;
+        } else if(v[i] == 'b') {
+            cout<<endl; cout<<"3";
+           matrix[x][y] = 1;
+           matrix[x][y+1] = 1;
+           matrix[x+1][y] = 1;
+           matrix[x+1][y+1] = 1;
+           cout<<v[i]<<"~";
+           i += 1;
+        }
+    }
+    if(v[i] == 'i') {
+        i++;
+        cout<<endl<<endl;
+        cout<<v[i]<<"...";
+        marime = marime/2;
+        matrice(x, y+marime, marime);
+        matrice(x, y, marime);
+        matrice(x+marime, y, marime);
+        matrice(x+marime, y+marime, marime);
+    }
+}
+
+int main() {
+
+    cin >> n;
+    for(int i = 0; i < 13; i++)
+        cin >> v[i];
+
+    matrice(0, 0, n);
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++)
+            cout << matrix[i][j] << " ";
+        cout << endl;
+    }
 }
