0f379f86df6be66f4be2c61b137c29877185ea60
Ana-Maria Melinte <34279579+AnMa12@users.noreply.github.com>
2018-06-03 04:50:41 +0300

    withFileReading
    8
    iiwbiwwbbiwbbbiwbbwbibwwb

--- LaboratoryTest/Ex1/main.cpp
+++ LaboratoryTest/Ex1/main.cpp
@@ -1,6 +1,6 @@
-#include<iostream>
+#include <iostream>
 #include <cmath>
-#include<fstream>
+#include <fstream>
 
 using namespace std;
 ifstream f("date.in");
@@ -9,67 +9,66 @@ int n, i = 0;
 char v[100];
 int matrix[100][100];
 
-int decodare(int litera) {
+int decodare(int litera)
+{
     if(litera == 'w')
         return 0;
     if(litera == 'b')
         return 1;
 }
 
-int matrice(int x, int y, int marime) {
+int matrice(int x, int y, int marime)
+{
 
-    if(v[i] == 'i') {
-        cout<<endl<<endl;
-        cout<<v[i]<<"...";
+    if(v[i] == 'i')
+    {
         i++;
-
         marime = marime/2;
         matrice(x, y+marime, marime);
         matrice(x, y, marime);
         matrice(x+marime, y, marime);
         matrice(x+marime, y+marime, marime);
     }
-    else {
-        if(v[i] == 'i') {
-                cout<<endl; cout<<"1";
-           matrix[x][y] = decodare(v[i+1]);
-           matrix[x][y+1] = decodare(v[i+2]);
-           matrix[x+1][y] = decodare(v[i+3]);
-           matrix[x+1][y+1] = decodare(v[i+4]);
-            for(int j = i; j < i+5; j++)
-                cout<<v[j]<< "-";
-           i += 5;
-        } else if(v[i] == 'w') {
-            cout<<endl;cout<<"2";
-
-           for(int j = x; j < x + marime; j++)
-            for(int q = y; q < y + marime; q++)
-                matrix[j][q] = 0;
-
-           cout<<v[i]<<"*";
-           i += 1;
-        } else if(v[i] == 'b') {
-            cout<<endl; cout<<"3";
-
-           for(int j = x; j < x + marime; j++)
-            for(int q = y; q < y + marime; q++)
-                matrix[j][q] = 1;
-           cout<<v[i]<<"~";
-           i += 1;
+    else
+    {
+        if(v[i] == 'i')
+        {
+            i += 5;
+            matrix[x][y] = decodare(v[i+1]);
+            matrix[x][y+1] = decodare(v[i+2]);
+            matrix[x+1][y] = decodare(v[i+3]);
+            matrix[x+1][y+1] = decodare(v[i+4]);
+        }
+        else if(v[i] == 'w')
+        {
+            i += 1;
+            for(int j = x; j < x + marime; j++)
+                for(int q = y; q < y + marime; q++)
+                    matrix[j][q] = 0;
+        }
+        else if(v[i] == 'b')
+        {
+            i += 1;
+            for(int j = x; j < x + marime; j++)
+                for(int q = y; q < y + marime; q++)
+                    matrix[j][q] = 1;
         }
     }
 }
 
-int main() {
+int main()
+{
 
-    cin >> n;
-    for(int i = 0; i < 30; i++)
-        cin >> v[i];
+    f >> n;
+    int iter = 0;
+    while(f >> v[iter]) {
+        iter++;
+    }
 
     matrice(0, 0, n);
 
-    cout<<endl;
-    for(int i = 0; i < n; i++) {
+    for(int i = 0; i < n; i++)
+    {
         for(int j = 0; j < n; j++)
             cout << matrix[i][j] << " ";
         cout << endl;
