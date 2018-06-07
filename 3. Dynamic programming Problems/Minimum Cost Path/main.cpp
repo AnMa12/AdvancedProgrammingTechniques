37fb37e7a6497f6303e2450c2c43281f48ebf54a
Ana-Maria Melinte <34279579+AnMa12@users.noreply.github.com>
2018-06-07 13:26:02 +0300

    Fixed minimum cost
    :red_circle: Beware at the fact that the matrix border is 0 so, 0 is the minim!!!!

--- 3. Dynamic programming Problems/Minimum Cost Path/main.cpp
+++ 3. Dynamic programming Problems/Minimum Cost Path/main.cpp
@@ -25,6 +25,21 @@ int minim(int a, int b)
     return b;
 }
 
+/*
+3 3
+2 5 4
+3 2 1
+4 5 10
+
+3 4
+1 3 5 8
+4 2 1 7
+4 3 2 3
+Min cost: 12
+The path: 1 3 2 1 2 3
+
+*/
+
 int main()
 {
     f >> n >> m;
@@ -48,15 +63,30 @@ int main()
     cout << "Min cost: " << pathM[n-1][m-1] << endl;
 
     //--- find the path ---//
+    int path[100];
+    int ip = 0;
     int i = n-1, j = m-1;
-    while(i!=0 || j!=0) {
-        cout<<"*"<<i<<"-"<<j<<"*  ";
-        cout << M[i][j] << endl;
-        if(pathM[i-1][j] < pathM[i][j-1])
+    while(i >= 0 && j >= 0)
+    {
+        //cout<<"*"<<i<<"-"<<j<<"*  ";
+        //cout << M[i][j] << endl;
+        path[ip++] = M[i][j];
+        if(pathM[i-1][j] == 0)
+            j--;
+        else if (pathM[i][j-1] == 0)
             i--;
         else
-            j--;
+        {
+            if(pathM[i-1][j] < pathM[i][j-1])
+                i--;
+            else
+                j--;
+        }
     }
+
+    cout << "The path: ";
+    for(int i = ip-1; i >= 0; i--)
+        cout << path[i] << " ";
 
     return 0;
 }
