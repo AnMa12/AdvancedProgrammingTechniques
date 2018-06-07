2ad3cf4f251f58f9ad7c88d28fc69ba744b5c6cc
Ana-Maria Melinte <34279579+AnMa12@users.noreply.github.com>
2018-06-07 23:13:00 +0300

    trying my best

--- LaboratoryTest/Ex2-2017/main.cpp
+++ LaboratoryTest/Ex2-2017/main.cpp
@@ -1,9 +1,47 @@
-#include <iostream>
-
+#include<iostream>
+#include<fstream>
+#include<stdlib.h>
+#include<math.h>
 using namespace std;
+ifstream f("date.in");
+int n, x;
+void divide(int n, int dim, int x, int y) {
 
+    if(dim = 1) {
+        cout << x+1 << " " << y+1 << endl;
+    }
+    else {
+        dim = dim/4;
+        cout<< "**" << n << "*" << dim << "**"<< endl;
+        if(n <= dim) {
+            //triunghiul din stanga sus
+            cout<<"a"<<endl;
+            divide(n, dim, x, y);
+        }
+
+        else if(n <= dim*2) {
+            cout<<"b"<<endl;
+            //triunghiul din dreapta jos
+            divide(n, dim, x + dim/2, y + dim/2);
+        }
+
+        else if(n <= dim*3) {
+            cout<<"c"<<endl;
+            //triunghiul din dreapta sus
+            divide(n, dim, x, y + dim/2);
+        }
+
+        else if(n <= dim*4) {
+            cout<<"d"<<endl;
+            //triunghiul din stanga jos
+            divide(n, dim, x + dim/2, y);
+        }
+
+    }
+}
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    f >> n >> x;
+    int dim = pow(2,n) * pow(2,n);
+    divide(x, dim, 0, 0);
 }
--- LaboratoryTest/Ex2-2018/main.cpp
+++ LaboratoryTest/Ex2-2018/main.cpp
@@ -5,18 +5,51 @@
 using namespace std;
 ifstream f("date.in");
 
+int k, n;
+
+void afisV(int v[100])
+{
+    for(int i = 0; i < n; i++)
+        cout << v[i] << " ";
+    cout << endl;
+}
+
 int main()
 {
-    int k, n, v[n];
     f >> k;
     f >> n;
-    int iter = 0;
-    while(f >> v[iter]) {
-        iter++;
+    int v[n];
+    for(int i = 0; i < n; i++)
+        f >> v[i];
+
+    int auxv[n];
+    for(int i = 0; i < n; i++)
+        auxv[i] = 1;
+
+    int i = 1, j = 0;
+    while(i <= n) {
+        j = 0;
+        i ++;
+        while(i != j){
+            if(abs(v[i] - v[j]) >= k)
+                auxv[i] = 1 + auxv[j];
+            j++;
+        }
     }
 
-    //subsir maxim de zigzag
+    int maxi = 0, nrMaxi = 0;
+    for(int i = 0; i < n; i++) {
+        if(auxv[i] > maxi) {
+            maxi = auxv[i];
+            nrMaxi = 1;
+        }
+        else if(auxv[i] == maxi) {
+            nrMaxi++;
+        }
+    }
 
+    afisV(auxv);
 
-
+    cout << maxi << endl;
+    cout << nrMaxi << endl;
 }
